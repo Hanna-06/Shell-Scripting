3d9b9592cd081dcf893b7373a2318d78c9c87c8d
Hanna-06 <hannasusan06@gmail.com>
2025-05-29 12:04:24 +0530

    Create running.sh

---
+++ running.sh
@@ -0,0 +1,10 @@
+#Shell script to check for any running shell scripts.
+
+#!/bin/bash
+proc=`ps aux | grep -e "bash\s\w*" | wc -l`
+if [[ $proc > 3 ]]
+then
+    echo "Running shell scripts exist."
+else
+    echo "No running shell scripts!!"
+fi
